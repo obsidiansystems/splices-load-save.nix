bb3e38150fe91be93a50a0268baaaa8dc91a3586
cidkidnix <cidkidnix@protonmail.com>
2022-06-23 20:33:45 -0500

    remove deps

--- default.nix
+++ default.nix
@@ -1,5 +1,13 @@
-{ haskellLib, fetchFromGitHub, lib, splicedHaskellPackages, isExternalPlugin }: {
-  loadSplices8_6 = null;
+#{ haskellLib, fetchFromGitHub, lib, splicedHaskellPackages, isExternalPlugin }: {
+{ ghc }: {
+  #loadSplices8_6 = import ./load-splices.nix { inherit haskellLib fetchFromGitHub lib splicedHaskellPackages isExternalPlugin; };
   loadSplices8_10 = null;
   saveSplices = null;
+
+  ghc810patched = ghc.overrideAttrs (old: {
+    patches = (old.patches or [ ]) ++ [
+      ./blank.patch
+    ];
+  });
+
 }
